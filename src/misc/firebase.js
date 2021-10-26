ef6f7bc4c563c93525993154a3f69a2085dd5ec1
byronicone <byron.e.barnes@gmail.com>
2021-10-26 11:40:25 -0400

    Initialized firebase app

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyDjI9Ps7bF5xlOzm5pSuvmq911V6zsCAHc',
+  authDomain: 'chat-web-app-ab9f9.firebaseapp.com',
+  projectId: 'chat-web-app-ab9f9',
+  storageBucket: 'chat-web-app-ab9f9.appspot.com',
+  messagingSenderId: '133018801781',
+  appId: '1:133018801781:web:9c6b451484630faf9f4a3c',
+};
+
+const app = firebase.initializeApp(config);
