d6406b80e637b972cabad98af76970061b6de61b
byronicone <byron.e.barnes@gmail.com>
2021-10-27 13:26:14 -0400

    Add sidebar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import DashboardToggle from './dashboard/DashboardToggle';
+
+const Sidebar = () => {
+  return (
+    <div className="ht-100 pt2">
+      <div>
+        <DashboardToggle />
+      </div>
+      bottom
+    </div>
+  );
+};
+
+export default Sidebar;
