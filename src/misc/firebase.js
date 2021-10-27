337fa6ca0a089ac6639f631d2a8a2cfe35e6c5f5
byronicone <byron.e.barnes@gmail.com>
2021-10-27 11:56:18 -0400

    Added sign in with Facebook / Google

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
   apiKey: 'AIzaSyDjI9Ps7bF5xlOzm5pSuvmq911V6zsCAHc',
@@ -10,3 +12,6 @@ const config = {
 };
 
 const app = firebase.initializeApp(config);
+
+export const auth = app.auth();
+export const database = app.database();
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,16 +1,49 @@
 import React from 'react';
-import { Col, Container, Grid, Panel, Row } from 'rsuite';
+import { Alert, Button, Col, Container, Grid, Icon, Panel, Row } from 'rsuite';
+import firebase from 'firebase/app';
+import { auth, database } from '../misc/firebase';
 
 const SignIn = () => {
+  const signInWithProvider = async provider => {
+    try {
+      const { additionalUserInfo, user } = await auth.signInWithPopup(provider);
+      if (additionalUserInfo.isNewUser) {
+        await database.ref(`/profiles/${user.uid}`).set({
+          name: user.displayName,
+          createdAt: firebase.database.ServerValue.TIMESTAMP,
+        });
+      }
+      Alert.success('Signed in', 4000);
+    } catch (e) {
+      Alert.error(e.message, 4000);
+    }
+  };
+
+  const signInWithFacebook = () => {
+    signInWithProvider(new firebase.auth.FacebookAuthProvider());
+  };
+
+  const signInWithGoogle = () => {
+    signInWithProvider(new firebase.auth.GoogleAuthProvider());
+  };
+
   return (
     <Container>
-      <Grid>
+      <Grid className="mt-page">
         <Row>
           <Col xs={24} md={12} mdOffset={6}>
             <Panel>
               <div className="text-center">
                 <h2>Welcome to Chat!</h2>
                 <p>Progressive chat platform for neophytes</p>
+                <div className="mt-3">
+                  <Button block color="blue" onClick={signInWithFacebook}>
+                    <Icon icon="facebook" /> Login with Facebook
+                  </Button>
+                  <Button block color="green" onClick={signInWithGoogle}>
+                    <Icon icon="google" /> Login with Google
+                  </Button>
+                </div>
               </div>
             </Panel>
           </Col>
