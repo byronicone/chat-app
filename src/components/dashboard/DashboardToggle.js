fda92b490db506214c1d01ed7e5ce8b23fe866c2
byronicone <byron.e.barnes@gmail.com>
2021-10-27 13:34:24 -0400

    Drawer component responsive to max width 992px

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Button, Drawer, Icon } from 'rsuite';
 import Dashboard from '.';
-import { useModalState } from '../../misc/custom-hooks';
+import { useMediaQuery, useModalState } from '../../misc/custom-hooks';
 
 const DashboardToggle = () => {
+  const isMobile = useMediaQuery('(max-width: 992px)');
   const [isOpen, open, close] = useModalState();
   return (
     <>
       <Button block color="blue" onClick={open}>
         <Icon icon="dashboard" /> Dashboard
       </Button>
-      <Drawer show={isOpen} onHide={close} placement="left">
+      <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
         <Dashboard />
       </Drawer>
     </>
