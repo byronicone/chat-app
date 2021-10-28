edb6435fe713fb25c848b02989f42442af90d956
byronicone <byron.e.barnes@gmail.com>
2021-10-28 12:05:43 -0400

    Created reusable editable input and used for nickname

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { Button, Drawer } from 'rsuite';
+import { Button, Divider, Drawer } from 'rsuite';
 import { useProfile } from '../../contexts/profile.context';
+import EditableInput from '../EditableInput';
 
 const Dashboard = ({ onSignOut }) => {
   const { profile } = useProfile();
+  const onSave = async newData => {
+    console.log(newData);
+  };
   return (
     <>
       <Drawer.Header>
@@ -11,6 +15,13 @@ const Dashboard = ({ onSignOut }) => {
       </Drawer.Header>
       <Drawer.Body>
         <h3>Hey, {profile.name}</h3>
+        <Divider />
+        <EditableInput
+          emptyMessage="Type here"
+          onSave={onSave}
+          initialValue={profile.name}
+          label={<h3 className="mb-3">Choose a nickname:</h3>}
+        />
       </Drawer.Body>
       <Drawer.Footer>
         <Button block color="red" onClick={onSignOut}>
