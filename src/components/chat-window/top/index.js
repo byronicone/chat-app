c502d642045aa113d78a6559175d7b24bf205e5d
byronicone <byron.e.barnes@gmail.com>
2021-11-03 11:14:34 -0400

    Chat top modal component to display room description

--- src/components/chat-window/top/index.js
+++ src/components/chat-window/top/index.js
@@ -1,9 +1,38 @@
 import React, { memo } from 'react';
+import { Icon } from 'rsuite';
+import { Link } from 'react-router-dom';
 import { useCurrentRoom } from '../../../contexts/current-room.context';
+import { useMediaQuery } from '../../../misc/custom-hooks';
+import RoomInfoBtnModal from './RoomInfoBtnModal';
 
 const ChatTop = () => {
   const name = useCurrentRoom(v => v.name);
-  return <div>{name}</div>;
+  const isMobile = useMediaQuery('(max-width: 992px)');
+
+  return (
+    <>
+      <div className="d-flex justify-content-between align-items-center">
+        <h4 className="text-disappear d-flex align-items-center">
+          <Icon
+            componentClass={Link}
+            to="/"
+            icon="arrow-circle-left"
+            size="2x"
+            className={
+              isMobile
+                ? 'd-inline-block p-0 mr-2 text-blue link-unstyled'
+                : 'd-none'
+            }
+          />
+          <span className="text-disappear">{name}</span>
+        </h4>
+      </div>
+
+      <div className="d-flex justify-content-between align-items-center">
+        <RoomInfoBtnModal />
+      </div>
+    </>
+  );
 };
 
 export default memo(ChatTop);
