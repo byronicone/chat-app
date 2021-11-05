b8a91a31a1936c5693dab58760a2071b1f05ea53
byronicone <byron.e.barnes@gmail.com>
2021-11-05 09:15:04 -0400

    Add real time presence listener on user login, close window, and sign out

--- src/contexts/profile.context.js
+++ src/contexts/profile.context.js
@@ -1,7 +1,18 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
+import firebase from 'firebase/app';
 import { auth, database } from '../misc/firebase';
 
 const ProfileContext = createContext();
+
+export const isOfflineForDatabase = {
+  state: 'offline',
+  last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
+
+const isOnlineForDatabase = {
+  state: 'online',
+  last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
 
 export const ProfileProvider = ({ children }) => {
   const [profile, setProfile] = useState(null);
@@ -9,9 +20,25 @@ export const ProfileProvider = ({ children }) => {
 
   useEffect(() => {
     let userRef;
+    let userStatusRef;
     const authUnsub = auth.onAuthStateChanged(authObj => {
       if (authObj) {
+        userStatusRef = database.ref(`/status/${authObj.uid}`);
         userRef = database.ref(`/profiles/${authObj.uid}`);
+
+        database.ref('.info/connected').on('value', snapshot => {
+          if (snapshot.val() === false) {
+            return;
+          }
+
+          userStatusRef
+            .onDisconnect()
+            .set(isOfflineForDatabase)
+            .then(() => {
+              userStatusRef.set(isOnlineForDatabase);
+            });
+        });
+
         userRef.on('value', snap => {
           setProfile({
             uid: authObj.uid,
@@ -26,6 +53,9 @@ export const ProfileProvider = ({ children }) => {
         if (userRef) {
           userRef.off();
         }
+        if (userStatusRef) {
+          userStatusRef.off();
+        }
         setProfile(null);
         setIsLoading(false);
       }
@@ -34,6 +64,9 @@ export const ProfileProvider = ({ children }) => {
       authUnsub();
       if (userRef) {
         userRef.off();
+      }
+      if (userStatusRef) {
+        userStatusRef.off();
       }
     };
   }, []);
