0dc92166c5b45faeadf1da958a8c5c9ceca78493
byronicone <byron.e.barnes@gmail.com>
2021-11-08 08:55:03 -0500

    Subtle background change to messages when hovering over them

--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { database } from './firebase';
 
 export const useModalState = (defaultValue = false) => {
@@ -48,4 +48,31 @@ export function usePresence(uid) {
   }, [uid]);
 
   return presence;
+}
+
+export function useHover() {
+  const [value, setValue] = useState(false);
+
+  const ref = useRef(null);
+
+  const handleMouseOver = () => setValue(true);
+  const handleMouseOut = () => setValue(false);
+
+  useEffect(
+    () => {
+      const node = ref.current;
+      if (node) {
+        node.addEventListener('mouseenter', handleMouseOver);
+        node.addEventListener('mouseleave', handleMouseOut);
+      }
+      return () => {
+        node.removeEventListener('mouseenter', handleMouseOver);
+        node.removeEventListener('mouseleave', handleMouseOut);
+      };
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [ref.current] // Recall only if ref changes
+  );
+
+  return [ref, value];
 }
