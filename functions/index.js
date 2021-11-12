4017558e7b97fc4e89d8dddef3c00b425a8bc08c
byronicone <byron.e.barnes@gmail.com>
2021-11-12 11:10:01 -0500

    Add fcm function to send notifications

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,3 @@
-const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
 const serviceAccount = require('./service-account.json');
@@ -7,10 +6,7 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://chat-web-app-ab9f9-default-rtdb.firebaseio.com',
 });
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+const { sendFcm } = require('./src/fcm');
+
+exports.sendFcm = sendFcm;
